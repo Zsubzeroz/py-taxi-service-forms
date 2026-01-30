713bcc6a1474177ebdfca0426d3e576e9873277e
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2026-01-29 21:28:39 -0300

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -3,17 +3,16 @@ from django.urls import reverse_lazy
 from .models import Car, Manufacturer
 from .forms import CarForm, ManufacturerForm
 
-from django.views.generic import ListView, DetailView # Assumindo que você tem ListView/DetailView importadas aqui
+from django.views.generic import ListView, DetailView # Importar outras Views que você usa
 
 
-# ... (suas views existentes como index, CarListView, etc.)
 
-# --- Views para Manufacturer (Criação, Atualização, Deleção) ---
-class ManufacturerCreateView(CreateView):
+class ManufacturerCreateView(CreateView): # E261: Garanta que não haja comentário indevido aqui ou adicione 2 espaços se houver
     model = Manufacturer
     form_class = ManufacturerForm
     template_name = "taxi/manufacturer_form.html"
     success_url = reverse_lazy("taxi:manufacturer-list")
+
 
 class ManufacturerUpdateView(UpdateView):
     model = Manufacturer
@@ -21,26 +20,18 @@ class ManufacturerUpdateView(UpdateView):
     template_name = "taxi/manufacturer_form.html"
     success_url = reverse_lazy("taxi:manufacturer-list")
 
+
 class ManufacturerDeleteView(DeleteView):
     model = Manufacturer
     template_name = "taxi/manufacturer_confirm_delete.html"
-    success_url = reverse_lazy("taxi:manufacturer-list")
+    success_url = reverse_lazy("taxi:manufacturer-list") # E501: Verifique se esta linha está muito longa (talvez não precise quebrar)
 
 
-# --- Views para Car (Criação, Atualização, Deleção) ---
+
+
 class CarCreateView(CreateView):
     model = Car
     form_class = CarForm
     template_name = "taxi/car_form.html"
     success_url = reverse_lazy("taxi:car-list")
 
-class CarUpdateView(UpdateView):
-    model = Car
-    form_class = CarForm
-    template_name = "taxi/car_form.html"
-    success_url = reverse_lazy("taxi:car-list")
-
-class CarDeleteView(DeleteView):
-    model = Car
-    template_name = "taxi/car_confirm_delete.html"
-    success_url = reverse_lazy("taxi:car-list")
